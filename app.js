8a26d48d06f52348d6e3f300da84503634931036
Emre Memiş <113227617+memisemre@users.noreply.github.com>
2022-12-15 12:08:51 +0300

    [Css Design] Design Clothe Areas

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const loginAnimationText = document.querySelector(".login-animation-container p");
+const appContainer = document.querySelector(".app-container");
 let counterNumber = 1;
 let loginAnimationTextFontSize = 16;
 const counterInterval = setInterval(()=>{
@@ -6,6 +7,9 @@ const counterInterval = setInterval(()=>{
       loginAnimationTextFontSize++;
       loginAnimationText.style.fontSize = `${loginAnimationTextFontSize}px`
       loginAnimationText.innerHTML = `${counterNumber}%`
-      if(counterNumber=== 100)clearInterval(counterInterval);
-                  
+      if(counterNumber=== 100){
+            clearInterval(counterInterval);
+            loginAnimationText.style.display = "none";
+            appContainer.style.display = "flex";
+      }
 },25);
